1f01ad1a3178826cd0471c190733f536f205ee5d
Mateusz Budzisz <mat.budzisz@gmail.com>
2021-08-18 12:07:21 +0200

    added routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+# 5.times do
+#   Restaurant.create!(name: Faker::Restaurant.name, category: CATEGORY.sample, address: Faker::Address.full_address)
+# end
